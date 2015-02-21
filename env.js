4178c6b752b7fc5cbb77b20211a61c3d447f6e88
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-02-21 21:13:23 +0200

    Fix current jshint issues - mostly quotes

--- env.js
+++ env.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  MONGODB: process.env.MONGOLAB_URI || process.env.MONGODB || "",
-  PHOTOSDATADIR: process.env.PHOTOSDATADIR || "photos",
-  KNOX_KEY: process.env.KNOX_KEY || "",
-  KNOX_SECRET: process.env.KNOX_SECRET || "",
-  KNOX_BUCKET: process.env.KNOX_BUCKET || "",
-  KNOX_REGION: process.env.KNOX_REGION || "",
-  TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY || "",
-  TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET || "",
-  TWITTER_TOKEN: process.env.TWITTER_TOKEN || "",
-  TWITTER_TOKEN_SECRET: process.env.TWITTER_TOKEN_SECRET || "",
-  USERNAME: process.env.USERNAME || "",
-  PASSWORD: process.env.PASSWORD || ""
+  MONGODB: process.env.MONGOLAB_URI || process.env.MONGODB || '',
+  PHOTOSDATADIR: process.env.PHOTOSDATADIR || 'photos',
+  KNOX_KEY: process.env.KNOX_KEY || '',
+  KNOX_SECRET: process.env.KNOX_SECRET || '',
+  KNOX_BUCKET: process.env.KNOX_BUCKET || '',
+  KNOX_REGION: process.env.KNOX_REGION || '',
+  TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY || '',
+  TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET || '',
+  TWITTER_TOKEN: process.env.TWITTER_TOKEN || '',
+  TWITTER_TOKEN_SECRET: process.env.TWITTER_TOKEN_SECRET || '',
+  USERNAME: process.env.USERNAME || '',
+  PASSWORD: process.env.PASSWORD || ''
 };
